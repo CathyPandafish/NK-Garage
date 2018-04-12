28534c780a2c4d4f39dd8ed6921631cc867e9559
Baryali Noori <baryalinoori786@yahoo.com>
2018-04-12 15:11:01 +0200

    Garage class collection

--- NKGarage/NKGarage/Garage.cs
+++ NKGarage/NKGarage/Garage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,43 @@ using System.Threading.Tasks;
 
 namespace NKGarage
 {
-    class Garage
+    public class Garage<T> : IEnumerable<T> where T : Vehicle
     {
+
+
+        private T[] Vihecle;
+        int cap, count;
+
+        public Garage(int capacity)
+        {
+            cap = capacity;
+            count = 0;
+            Vihecle = new T[capacity];
+
+        }
+
+        public void Leave(T input)
+        {
+            if (cap > count)
+            {
+                Vihecle[count] = input;
+                count += 1;
+            }
+
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return Vihecle[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
