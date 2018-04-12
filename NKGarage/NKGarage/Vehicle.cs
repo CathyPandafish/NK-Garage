5af392da8cb5cf8e98cb27c1c29059a0dade6769
Baryali Noori <baryalinoori786@yahoo.com>
2018-04-12 16:23:31 +0200

    done Vehicle

--- NKGarage/NKGarage/Vehicle.cs
+++ NKGarage/NKGarage/Vehicle.cs
@@ -8,9 +8,21 @@ namespace NKGarage
 {
    public class Vehicle
     {
-        public Vehicle()
+        public Vehicle(int registrationNumber, string color , int nrOfWheels, int doors, string brand)
         {
+            RegistrationNumber = registrationNumber;
+            Color = color;
+            NrofWheels = nrOfWheels;
+            Doors = doors;
+            Brand = brand;
 
         }
+
+        public int RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public int NrofWheels { get; set; }
+        public int Doors { get; set; }
+        public string Brand { get; set; }
+
     }
 }
